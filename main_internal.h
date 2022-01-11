55df37431392914459bfa7941ca63c6ace09528e
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-01-11 18:00:53 +0530

    Create main_internal.h

---
+++ main_internal.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_INTERNAL_H
+#define MAIN_INTERNAL_H
+/*
+ **********************************************************************************************************************
+ * Type definitions
+ **********************************************************************************************************************
+ */
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+#endif /* MAIN_INTERNAL_H */
