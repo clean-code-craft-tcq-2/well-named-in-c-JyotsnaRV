a8c513f5c8092322c29db0449d8e77536d72d748
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-01-25 15:08:57 +0530

    Correct function typo

--- main.h
+++ main.h
@@ -31,6 +31,6 @@ extern ColorPair GetColorFromPairNumber(int pairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
 extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
-extern void printRefernceManual(void);
+extern void printReferenceManual(void);
 
 #endif /* MAIN_H */
